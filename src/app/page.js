3f1da024e1d0ed5001a9fccd9157a03b1fbeed98
RinaldoDwiFaturahman <faturahmanrinaldo@gmail.com>
2023-06-22 23:03:03 +0700

    Sidebar Component

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+'use client'
+import Aside from "@/components/Aside"
+import Navbar from "@/components/Navbar"
+
+export default function Home() {
+  return (
+    <main>
+      <Aside/>
+      {/* <Navbar/> */}
+    </main>
+  )
+}
