ede0b9cef81af765c924d710feca5410710a25c7
marioutomousm <marioutomo.usm@gmail.com>
2023-07-06 14:44:34 +0700

    Change form flight add

--- src/app/flight.new/page.js
+++ src/app/flight.new/page.js
@@ -4,16 +4,18 @@ import FormFlightAdd from '@/components/FormFlightAdd';
 import Aside from '@/components/Aside';
 import TopComponent from '@/components/TopComponent';
 import { useState, useEffect } from 'react';
-import { useRouter } from "next/navigation";
-
+import { useRouter } from 'next/navigation';
 
 export default function FlightAdd() {
     const [isMounted, setIsMounted] = useState(false);
     const router = useRouter();
+
+    
+
     useEffect(() => {
         setIsMounted(true);
     }, []);
-    if (isMounted) { 
+    if (isMounted) {
         return (
             <section className='h-[950px] w-[1440px] bg-grey-2  '>
                 <nav className=''>
@@ -27,17 +29,20 @@ export default function FlightAdd() {
                             <div className='ml-[361px] mt-[47px]'>
                                 <div>
                                     <div className='flex'>
-                                        <h1 className=' text-[32px] font-bold text-blue-1 w-[105px]'>Flight</h1>
-                                        <div className='ml-[262px]'>
-                                            {/* <TopComponent /> */}
-                                        </div>
+                                        <h1 className=' w-[105px] text-[32px] font-bold text-blue-1'>Flight</h1>
+                                        <div className='ml-[262px]'>{/* <TopComponent /> */}</div>
                                     </div>
                                 </div>
                             </div>
                             {/* FORM */}
                             <div className='ml-[351px] mt-[77px] '>
                                 <div className='flex'>
-                                    <img onClick={() => router.push('/flight')} className=' cursor-pointer' src={`./images/back.svg`} alt='' />
+                                    <img
+                                        onClick={() => router.push('/flight')}
+                                        className=' cursor-pointer'
+                                        src={`./images/back.svg`}
+                                        alt=''
+                                    />
                                     <h1 className='ml-[12px] text-[21px] font-bold text-blue-1 '>New Flight</h1>
                                     <div className='ml-[670px] '></div>
                                 </div>
@@ -50,6 +55,5 @@ export default function FlightAdd() {
                 </nav>
             </section>
         );
-    
     }
 }
